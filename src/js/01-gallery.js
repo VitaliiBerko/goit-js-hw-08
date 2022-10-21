d82d54295c0387ea6fea6f8a2361988d75fc7300
Vitalii Berko <vitalii.v.berko@gmail.com>
2022-10-21 15:27:36 +0300

    Added task3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,28 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-import SimpleLightbox from "simplelightbox";
+// import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import galleryTmp from '../templates/gallery.hbs'
 
-console.log(galleryItems);
+// console.log(galleryTmp);
+
+// console.log(galleryItems);
 
 
 const galleryContainer = document.querySelector('.gallery');
-const cardsMarkUp = createPhotoCartMarkup(galleryItems);
+const cardsMarkUp = createPhotoCardMarkup(galleryItems);
 
 
 galleryContainer.insertAdjacentHTML('beforeend', cardsMarkUp);
 
 galleryContainer.addEventListener('click', onGalleryContainerClick);
 
-function createPhotoCartMarkup() {
-    return galleryItems.map( ({preview, original, description}) => {
-        return `
-        <a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>
-    `
-    }).join('');     
+function createPhotoCardMarkup(galleryItems) {
+    return galleryItems.map(galleryTmp).join('');     
 }
 
 const lightBox = new SimpleLightbox('.gallery a', {
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,30 +1,40 @@
 
-const ref = {
+import throttle from "lodash.throttle";
+
+const refs = {
     form: document.querySelector('.feedback-form'),
     email: document.querySelector('[type="email"]'),
     message: document.querySelector('[name="message"]'),
-    submit: document.querySelector('button'),
+    btn: document.querySelector('button'),
 }
 
-// const form = document.querySelector('.feedback-form');
-// console.log(ref.form);
-// // const email = form.querySelector('[type="email"]');
-// console.log(ref.email);
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
 
-// // const message = form.querySelector('[name="message"]');
-// console.log(ref.message);
-// console.log(ref.submit);
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
 
+populateText();
 
-ref.form.addEventListener('input', handleInput);
+function onFormSubmit(e) { 
+    e.preventDefault();    
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);    
+    console.log(formData);
+};
 
-function handleInput(e) {
+function onFormInput(e) {    
+    formData[e.target.name] = e.target.value;    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
 }
 
-ref.submit.addEventListener('submit', onSubmitClick);
+function populateText() {
 
-function onSubmitClick(e) {
-    e.prevenDefault();
-    
-    
+    const saveMssage = JSON.parse(localStorage.getItem(STORAGE_KEY));    
+
+    if(saveMssage) {
+        refs.email.value = saveMssage.email;
+        refs.message.value = saveMssage.message;
+    }
 }
+
