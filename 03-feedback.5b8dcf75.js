1ac8afec44a99f52288ffcea7cd3223ed8d1e496
VitaliiBerko <106317469+VitaliiBerko@users.noreply.github.com>
2022-10-24 08:54:32 +0000

    Deploying to gh-pages from @ VitaliiBerko/goit-js-hw-08@133f0bdf2c51f79b7c32f31375bdfa0acb6af6a3 🚀

--- 03-feedback.5b8dcf75.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={form:document.querySelector(".feedback-form"),email:document.querySelector('[type="email"]'),message:document.querySelector('[name="message"]'),submit:document.querySelector("button")};e.form.addEventListener("input",(function(e){})),e.submit.addEventListener("submit",(function(e){e.prevenDefault()}))}();
-//# sourceMappingURL=03-feedback.5b8dcf75.js.map
